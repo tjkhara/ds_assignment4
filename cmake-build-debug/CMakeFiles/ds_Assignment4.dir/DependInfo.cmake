9c31f271de209183cc671477e8fb2a5c7e7522c3
Tajeshwar Khara <tkhara@gmail.com>
2019-05-02 09:33:06 +0530

    Assignment 4 first commit

---
+++ cmake-build-debug/CMakeFiles/ds_Assignment4.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/tkhara/CLionProjects/ds_Assignment4/bst.c" "/Users/tkhara/CLionProjects/ds_Assignment4/cmake-build-debug/CMakeFiles/ds_Assignment4.dir/bst.c.o"
+  "/Users/tkhara/CLionProjects/ds_Assignment4/compare.c" "/Users/tkhara/CLionProjects/ds_Assignment4/cmake-build-debug/CMakeFiles/ds_Assignment4.dir/compare.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ds_Assignment4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ds_Assignment4.dir/bst.c.o"
+  "CMakeFiles/ds_Assignment4.dir/compare.c.o"
+  "ds_Assignment4.pdb"
+  "ds_Assignment4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ds_Assignment4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
